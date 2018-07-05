a51abe13632ca18b5e5988c4cf35563955dea17a
Ryan Manchester <ryanm17@gmail.com>
2018-07-05 21:15:17 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -29,10 +29,11 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/posts/:id/edit' do
+    @post = Post.find(params[:id])
     erb :edit
   end
 
   patch '/posts/:id' do
-    
+
   end
 end
